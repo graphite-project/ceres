f35df71f3214636306d1f857667d93fa1f4065f6
Chris Davis <chrismd@gmail.com>
2011-04-18 16:08:10 -0500

    fixed many issues, implemented working rollup plugin for ceres-maintenance

---
+++ plugins/template.py
@@ -0,0 +1,13 @@
+# Magical Variables
+#-------------------
+# log(message)
+# context = {}
+
+
+
+#def maintenance_start(tree):
+#def maintenance_complete(tree):
+
+#def node_found(node):
+#def directory_found(path):
+#def directory_empty(path):
