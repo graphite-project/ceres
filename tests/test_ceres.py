43c61c8c0de572f995731254ffd26f1255f9c2e8
Michael Leinartas <mleinartas@twitter.com>
2013-08-24 17:35:50 -0500

    additional tests for CeresNode

--- tests/test_ceres.py
+++ tests/test_ceres.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from mock import ANY, Mock, mock_open, patch
+from mock import ANY, Mock, call, mock_open, patch
 
 from ceres import *
 
@@ -223,6 +223,21 @@ class CeresNodeTest(TestCase):
       with patch('ceres.exists', new=Mock(return_value=True)):
         self.ceres_tree = CeresTree('/graphite/storage/ceres')
         self.ceres_node = CeresNode(self.ceres_tree, 'sample_metric', '/graphite/storage/ceres/sample_metric')
+        self.ceres_node.timeStep = 60
+
+    slice_configs = [
+      ( 1200, 1800, 60 ),
+      ( 600, 1200, 60 )]
+
+    self.ceres_slices = []
+    for start, end, step in slice_configs:
+      slice_mock = Mock(spec=CeresSlice)
+      slice_mock.startTime = start
+      slice_mock.endTime = end
+      slice_mock.timeStep = step
+
+      self.ceres_slices.append(slice_mock)
+
 
   def test_init_sets_default_cache_behavior(self):
     ceres_node = CeresNode(self.ceres_tree, 'sample_metric', '/graphite/storage/ceres/sample_metric')
@@ -356,6 +371,148 @@ class CeresNodeTest(TestCase):
     listdir_mock = Mock(return_value=['0@60.slice', '0@300.slice', 'foo'])
     with patch('ceres.os.listdir', new=listdir_mock):
       self.assertEquals(2, len(self.ceres_node.readSlices()))
+
+  @patch('ceres.exists', new=Mock(return_Value=True))
+  def test_read_slices_parses_slice_filenames(self):
+    listdir_mock = Mock(return_value=['0@60.slice', '0@300.slice'])
+    with patch('ceres.os.listdir', new=listdir_mock):
+      slice_infos = self.ceres_node.readSlices()
+      self.assertTrue((0,60) in slice_infos)
+      self.assertTrue((0,300) in slice_infos)
+
+  @patch('ceres.exists', new=Mock(return_Value=True))
+  def test_read_slices_reverse_sorts_by_time(self):
+    listdir_mock = Mock(return_value=[
+      '0@60.slice',
+      '320@300.slice',
+      '120@120.slice',
+      '0@120.slice',
+      '600@300.slice'])
+
+    with patch('ceres.os.listdir', new=listdir_mock):
+      slice_infos = self.ceres_node.readSlices()
+      slice_timestamps = [ s[0] for s in slice_infos ]
+      self.assertEqual([600,320,120,0,0], slice_timestamps)
+
+  def test_no_data_exists_if_no_slices_exist(self):
+    with patch('ceres.CeresNode.readSlices', new=Mock(return_value=[])):
+      self.assertFalse(self.ceres_node.hasDataForInterval(0,60))
+
+  def test_no_data_exists_if_no_slices_exist_and_no_time_specified(self):
+    with patch('ceres.CeresNode.readSlices', new=Mock(return_value=[])):
+      self.assertFalse(self.ceres_node.hasDataForInterval(None,None))
+
+  def test_data_exists_if_slices_exist_and_no_time_specified(self):
+    with patch('ceres.CeresNode.slices', new=self.ceres_slices):
+      self.assertTrue(self.ceres_node.hasDataForInterval(None,None))
+
+  def test_data_exists_if_slice_covers_interval_completely(self):
+    with patch('ceres.CeresNode.slices', new=[self.ceres_slices[0]]):
+      self.assertTrue(self.ceres_node.hasDataForInterval(1200,1800))
+
+  def test_data_exists_if_slice_covers_interval_end(self):
+    with patch('ceres.CeresNode.slices', new=[self.ceres_slices[0]]):
+      self.assertTrue(self.ceres_node.hasDataForInterval(600, 1260))
+
+  def test_data_exists_if_slice_covers_interval_start(self):
+    with patch('ceres.CeresNode.slices', new=[self.ceres_slices[0]]):
+      self.assertTrue(self.ceres_node.hasDataForInterval(1740, 2100))
+
+  def test_no_data_exists_if_slice_touches_interval_end(self):
+    with patch('ceres.CeresNode.slices', new=[self.ceres_slices[0]]):
+      self.assertFalse(self.ceres_node.hasDataForInterval(600, 1200))
+
+  def test_no_data_exists_if_slice_touches_interval_start(self):
+    with patch('ceres.CeresNode.slices', new=[self.ceres_slices[0]]):
+      self.assertFalse(self.ceres_node.hasDataForInterval(1800, 2100))
+
+  def test_compact_returns_empty_if_passed_empty(self):
+    self.assertEqual([], self.ceres_node.compact([]))
+
+  def test_compact_filters_null_values(self):
+    self.assertEqual([], self.ceres_node.compact([(60,None)]))
+
+  def test_compact_rounds_timestamps_down_to_step(self):
+    self.assertEqual([[(600,0)]], self.ceres_node.compact([(605,0)]))
+
+  def test_compact_drops_duplicate_timestamps(self):
+    datapoints = [ (600, 0), (600, 0) ]
+    compacted = self.ceres_node.compact(datapoints)
+    self.assertEqual([[(600, 0)]], compacted)
+
+  def test_compact_groups_contiguous_points(self):
+    datapoints = [ (600, 0), (660, 0), (840,0) ]
+    compacted = self.ceres_node.compact(datapoints)
+    self.assertEqual([[(600, 0), (660,0)], [(840,0)]], compacted)
+
+  def test_write_noops_if_no_datapoints(self):
+    with patch('ceres.CeresNode.slices', new=self.ceres_slices):
+      self.ceres_node.write([])
+      self.assertFalse(self.ceres_slices[0].write.called)
+
+  def test_write_within_first_slice(self):
+    datapoints = [(1200, 0.0), (1260, 1.0), (1320, 2.0)]
+
+    with patch('ceres.CeresNode.slices', new=self.ceres_slices):
+      self.ceres_node.write(datapoints)
+      self.ceres_slices[0].write.assert_called_once_with(datapoints)
+
+  @patch('ceres.CeresSlice.create')
+  def test_write_within_first_slice_doesnt_create(self, slice_create_mock):
+    datapoints = [(1200, 0.0), (1260, 1.0), (1320, 2.0)]
+
+    with patch('ceres.CeresNode.slices', new=self.ceres_slices):
+      self.ceres_node.write(datapoints)
+      self.assertFalse(slice_create_mock.called)
+
+  @patch('ceres.CeresSlice.create', new=Mock())
+  def test_write_within_first_slice_with_gaps(self):
+    datapoints = [ (1200,0.0), (1320,2.0) ]
+
+    with patch('ceres.CeresNode.slices', new=self.ceres_slices):
+      self.ceres_node.write(datapoints)
+
+      # sorted most recent first
+      calls = [call.write([datapoints[1]]), call.write([datapoints[0]])]
+      self.ceres_slices[0].assert_has_calls(calls)
+
+  @patch('ceres.CeresSlice.create', new=Mock())
+  def test_write_within_previous_slice(self):
+    datapoints = [ (720,0.0), (780,2.0) ]
+
+    with patch('ceres.CeresNode.slices', new=self.ceres_slices):
+      self.ceres_node.write(datapoints)
+
+      # 2nd slice has this range
+      self.ceres_slices[1].write.assert_called_once_with(datapoints)
+
+  @patch('ceres.CeresSlice.create', new=Mock())
+  def test_write_within_previous_slice_with_gaps(self):
+    datapoints = [ (720,0.0), (840,2.0) ]
+
+    with patch('ceres.CeresNode.slices', new=self.ceres_slices):
+      self.ceres_node.write(datapoints)
+
+      calls = [call.write([datapoints[1]]), call.write([datapoints[0]])]
+      self.ceres_slices[1].assert_has_calls(calls)
+
+  @patch('ceres.CeresSlice.create', new=Mock())
+  def test_write_across_slice_boundaries(self):
+    datapoints = [ (1080,0.0), (1140,1.0), (1200, 2.0), (1260, 3.0) ]
+
+    with patch('ceres.CeresNode.slices', new=self.ceres_slices):
+      self.ceres_node.write(datapoints)
+      self.ceres_slices[0].write.assert_called_once_with(datapoints[2:4])
+      self.ceres_slices[1].write.assert_called_once_with(datapoints[0:2])
+
+  @patch('ceres.CeresSlice.create')
+  def test_create_during_write_clears_slice_cache(self, slice_create_mock):
+    self.ceres_node.setSliceCachingBehavior('all')
+    self.ceres_node.sliceCache = self.ceres_slices
+    datapoints = [ (300, 0.0) ]
+    with patch('ceres.CeresNode.slices', new=self.ceres_slices):
+      self.ceres_node.write(datapoints)
+      self.assertEquals(None, self.ceres_node.sliceCache)
 
 
 class CeresSliceTest(TestCase):
